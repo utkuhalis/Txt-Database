41543448c0f7a7080b26644fe746807d096bf804
Utku Halis <20300056@emu.edu.tr>
2021-04-22 13:29:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Txt-Database
+ This project is a project I created in my development process in 2014.
---
+++ txt Doyasını Veritabanı Yapmak/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Dim user As New RichTextBox
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If My.Computer.FileSystem.FileExists(Application.StartupPath & "/veri/" & TextBox1.Text & ".txt") = True Then
+            user.LoadFile(Application.StartupPath & "/veri/" & TextBox1.Text & ".txt", RichTextBoxStreamType.PlainText)
+            TextBox1.Text = user.Lines(0).Split("="c)(user.Lines(0).Split("="c).Length - 1)
+            TextBox2.Text = user.Lines(1).Split("="c)(user.Lines(1).Split("="c).Length - 1)
+        Else
+            MsgBox("Kullanıcı Bulunamadı.", MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        user.Text = "=" & TextBox1.Text & vbNewLine & "=" & TextBox2.Text
+        user.SaveFile(Application.StartupPath & "/veri/" & TextBox1.Text & ".txt", RichTextBoxStreamType.PlainText)
+        RichTextBox1.Text = RichTextBox1.Text & vbNewLine & TextBox1.Text
+        RichTextBox1.SaveFile(Application.StartupPath & "/veri/veri.txt", RichTextBoxStreamType.PlainText)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If My.Computer.FileSystem.FileExists(Application.StartupPath & "/veri/" & TextBox1.Text & ".txt") = True Then
+            If MsgBox("Emin Misiniz?", MsgBoxStyle.YesNo, TextBox1.Text & " Silinsin mi?") = MsgBoxResult.Yes Then
+                My.Computer.FileSystem.DeleteFile(Application.StartupPath & "/veri/" & TextBox1.Text & ".txt")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        RichTextBox1.LoadFile(Application.StartupPath & "/veri/veri.txt", RichTextBoxStreamType.PlainText)
+    End Sub
+End Class
